5cce9d8c32fca7498c622bdc6b64e2cd49ea12c9
Bryson Tanner <bltanner105@gmail.com>
2023-09-14 21:55:32 -0500

    test: Add tests for pretty unit types

--- test-poids/test-metric.cpp
+++ test-poids/test-metric.cpp
@@ -6,7 +6,7 @@ import poids.metric;
   TEST(TestLiteralsMetric, name) {                                           \
     expectedType expected{expectedValue};                                    \
     auto actual = actualValue;                                               \
-    EXPECT_EQ(expected.dimension, actual.dimension);                                   \
+    EXPECT_EQ(expected.dimension, actual.dimension);                         \
     EXPECT_NEAR(expected.base(), actual.base(), 1e-10);                      \
   }
 
@@ -18,3 +18,12 @@ METRIC_LITERALS_TEST(Kelvin, poids::Unit<poids::TemperatureD(1)>, 1.0, poids::me
 METRIC_LITERALS_TEST(Ampere, poids::Unit<poids::CurrentD(1)>, 1.0, poids::metric::ampere);
 METRIC_LITERALS_TEST(Mole, poids::Unit<poids::AmountD(1)>, 1.0, poids::metric::mole);
 METRIC_LITERALS_TEST(Candela, poids::Unit<poids::LuminosityD(1)>, 1.0, poids::metric::candela);
+
+// Pretty unit types
+METRIC_LITERALS_TEST(PrettySecond, poids::Time, 1.0, poids::metric::second);
+METRIC_LITERALS_TEST(PrettyKilogram, poids::Mass, 1.0, poids::metric::kilogram);
+METRIC_LITERALS_TEST(PrettyMeter, poids::Length, 1.0, poids::metric::meter);
+METRIC_LITERALS_TEST(PrettyKelvin, poids::Temperature, 1.0, poids::metric::kelvin);
+METRIC_LITERALS_TEST(PrettyAmpere, poids::Current, 1.0, poids::metric::ampere);
+METRIC_LITERALS_TEST(PrettyMole, poids::Amount, 1.0, poids::metric::mole);
+METRIC_LITERALS_TEST(PrettyCandela, poids::Luminosity, 1.0, poids::metric::candela);

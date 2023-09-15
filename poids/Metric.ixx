dc3331605abf6f865dede4ed971da0324379e535
Bryson Tanner <bltanner105@gmail.com>
2023-09-14 22:35:51 -0500

    feat: Implement useful secondary units

--- poids/Metric.ixx
+++ poids/Metric.ixx
@@ -4,11 +4,34 @@ export import poids.unit;
 export import poids.unit_types;
 
 namespace poids::metric {
-  export const ::poids::Time second{1};
-  export const ::poids::Mass kilogram{1};
-  export const ::poids::Length meter{1};
-  export const ::poids::Temperature kelvin{1};
-  export const ::poids::Current ampere{1};
-  export const ::poids::Amount mole{1};
-  export const ::poids::Luminosity candela{1};
+  export const Time second{1};
+  export const Mass kilogram{1};
+  export const Length meter{1};
+  export const Temperature kelvin{1};
+  export const Current ampere{1};
+  export const Amount mole{1};
+  export const Luminosity candela{1};
+
+  export const Mass gram{0.001};
+
+  export const Unit<TimeD(2)> second2{1};
+  export const Unit<TimeD(3)> second3{1};
+  export const Frequency hertz{1};
+
+  export const Area meter2{1};
+  export const Volume meter3{1};
+  export const SecondMomentOfArea meter4{1};
+
+  export const Force newton{1};
+  export const Energy joule{1};
+  export const Power watt{1};
+
+  export const Charge coulomb{1};
+  export const Voltage volt{1};
+  export const Resistance ohm{1};
+  export const Capacitance farad{1};
+  export const Inductance henry{1};
+
+  export const Pressure pascal{1};
+
 }  // namespace poids::metric
--- test-poids/test-metric.cpp
+++ test-poids/test-metric.cpp
@@ -11,19 +11,48 @@ import poids.metric;
   }
 
 // Base Unit tests
-METRIC_LITERALS_TEST(Second, poids::Unit<poids::TimeD(1)>, 1.0, poids::metric::second);
-METRIC_LITERALS_TEST(Kilogram, poids::Unit<poids::MassD(1)>, 1.0, poids::metric::kilogram);
-METRIC_LITERALS_TEST(Meter, poids::Unit<poids::LengthD(1)>, 1.0, poids::metric::meter);
-METRIC_LITERALS_TEST(Kelvin, poids::Unit<poids::TemperatureD(1)>, 1.0, poids::metric::kelvin);
-METRIC_LITERALS_TEST(Ampere, poids::Unit<poids::CurrentD(1)>, 1.0, poids::metric::ampere);
-METRIC_LITERALS_TEST(Mole, poids::Unit<poids::AmountD(1)>, 1.0, poids::metric::mole);
-METRIC_LITERALS_TEST(Candela, poids::Unit<poids::LuminosityD(1)>, 1.0, poids::metric::candela);
+METRIC_LITERALS_TEST(Second, poids::Unit<poids::TimeD(1)>, 1.0, poids::metric::second)
+METRIC_LITERALS_TEST(Kilogram, poids::Unit<poids::MassD(1)>, 1.0, poids::metric::kilogram)
+METRIC_LITERALS_TEST(Meter, poids::Unit<poids::LengthD(1)>, 1.0, poids::metric::meter)
+METRIC_LITERALS_TEST(Kelvin, poids::Unit<poids::TemperatureD(1)>, 1.0, poids::metric::kelvin)
+METRIC_LITERALS_TEST(Ampere, poids::Unit<poids::CurrentD(1)>, 1.0, poids::metric::ampere)
+METRIC_LITERALS_TEST(Mole, poids::Unit<poids::AmountD(1)>, 1.0, poids::metric::mole)
+METRIC_LITERALS_TEST(Candela, poids::Unit<poids::LuminosityD(1)>, 1.0, poids::metric::candela)
 
 // Pretty unit types
-METRIC_LITERALS_TEST(PrettySecond, poids::Time, 1.0, poids::metric::second);
-METRIC_LITERALS_TEST(PrettyKilogram, poids::Mass, 1.0, poids::metric::kilogram);
-METRIC_LITERALS_TEST(PrettyMeter, poids::Length, 1.0, poids::metric::meter);
-METRIC_LITERALS_TEST(PrettyKelvin, poids::Temperature, 1.0, poids::metric::kelvin);
-METRIC_LITERALS_TEST(PrettyAmpere, poids::Current, 1.0, poids::metric::ampere);
-METRIC_LITERALS_TEST(PrettyMole, poids::Amount, 1.0, poids::metric::mole);
-METRIC_LITERALS_TEST(PrettyCandela, poids::Luminosity, 1.0, poids::metric::candela);
+METRIC_LITERALS_TEST(PrettySecond, poids::Time, 1.0, poids::metric::second)
+METRIC_LITERALS_TEST(PrettyKilogram, poids::Mass, 1.0, poids::metric::kilogram)
+METRIC_LITERALS_TEST(PrettyMeter, poids::Length, 1.0, poids::metric::meter)
+METRIC_LITERALS_TEST(PrettyKelvin, poids::Temperature, 1.0, poids::metric::kelvin)
+METRIC_LITERALS_TEST(PrettyAmpere, poids::Current, 1.0, poids::metric::ampere)
+METRIC_LITERALS_TEST(PrettyMole, poids::Amount, 1.0, poids::metric::mole)
+METRIC_LITERALS_TEST(PrettyCandela, poids::Luminosity, 1.0, poids::metric::candela)
+
+// Secondary units tests
+METRIC_LITERALS_TEST(Gram, poids::Mass, 0.001, poids::metric::gram);
+
+METRIC_LITERALS_TEST(Hertz, poids::Frequency, 3.5, 3.5 * poids::metric::hertz)
+
+METRIC_LITERALS_TEST(MetersSquared, poids::Area, 6.78, 6.78 * poids::metric::meter2)
+METRIC_LITERALS_TEST(MetersCubed, poids::Volume, -9.043, -9.043 * poids::metric::meter3)
+METRIC_LITERALS_TEST(MetersFourth, poids::SecondMomentOfArea, 6.7e8, 6.7e8 * poids::metric::meter4)
+
+METRIC_LITERALS_TEST(MetersPerSecond, poids::Velocity, 6.67, 6.67 * poids::metric::meter / poids::metric::second)
+METRIC_LITERALS_TEST(MetersPerSecondSquared, poids::Acceleration, 0.4, 0.4 * poids::metric::meter / poids::metric::second2)
+METRIC_LITERALS_TEST(MetersPerSecondCubed, poids::Jerk, 1.1, 1.1 * poids::metric::meter / poids::metric::second3)
+
+METRIC_LITERALS_TEST(Newtons, poids::Force, -5, -5 * poids::metric::newton)
+METRIC_LITERALS_TEST(Joules, poids::Energy, 8.8, 8.8 * poids::metric::joule)
+METRIC_LITERALS_TEST(Watts, poids::Power, 0.456, 0.456 * poids::metric::watt)
+
+METRIC_LITERALS_TEST(Coulombs, poids::Charge, 12, 12 * poids::metric::coulomb)
+METRIC_LITERALS_TEST(Volts, poids::Voltage, -5.5, -5.5 * poids::metric::volt)
+METRIC_LITERALS_TEST(Ohms, poids::Resistance, 6.5e2, 6.5e2 * poids::metric::ohm)
+METRIC_LITERALS_TEST(Farads, poids::Capacitance, 43e-5, 43e-5 * poids::metric::farad)
+METRIC_LITERALS_TEST(Henries, poids::Inductance, 0.34e-2, 0.34e-2 * poids::metric::henry)
+
+METRIC_LITERALS_TEST(KilogramPerCubicMeter, poids::Density, 3.23, 3.23 * poids::metric::kilogram / poids::metric::meter3)
+METRIC_LITERALS_TEST(Pascals, poids::Pressure, 5.6e4, 5.6e4 * poids::metric::pascal)
+METRIC_LITERALS_TEST(KilogramPerMole, poids::MolarMass, 3.23e-4, 3.23e-4 * poids::metric::kilogram / poids::metric::mole)
+METRIC_LITERALS_TEST(PascalSecond, poids::DynamicViscosity, 28.1, 28.1 * poids::metric::pascal * poids::metric::second)
+METRIC_LITERALS_TEST(JouleSecondPerKilogram, poids::KinematicViscosity, 0.23, 0.23 * poids::metric::meter2 / poids::metric::second)

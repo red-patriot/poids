6e4b43822d6df827e7d99f3ded0e0c2292b78d78
Bryson Tanner <sandwichman1618@gmail.com>
2024-12-06 09:52:37 -0600

    docs: Add documentation for kgms::UnitType

--- include/poids/kgms/unit.hpp
+++ include/poids/kgms/unit.hpp
@@ -2,15 +2,23 @@
 #define POIDS_KGMS_UNIT_HPP
 
 namespace kgms {
+  /**
+   * A Unit in the KGMS system
+   *
+   * \tparam MassRatio The quantity of the kilograms unit dimension
+   * \tparam LengthRatio The quantity of the meters unit dimension
+   * \tparam TimeRatio The quantity of the seconds unit dimension
+   */
   template <typename MassRatio,
             typename LengthRatio,
             typename TimeRatio>
   struct UnitType {
-    using mass = MassRatio;
-    using length = LengthRatio;
-    using time = TimeRatio;
+    using mass = MassRatio;     /**< The quantity of mass units*/
+    using length = LengthRatio; /**< The quantity of length units*/
+    using time = TimeRatio;     /**< The quantity of time units*/
 
    private:
+    /** Implements multiplying this UnitType with another*/
     template <typename Other>
     struct MultiplyImpl {
       using type = UnitType<std::ratio_add<mass,

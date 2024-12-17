8d5337d6f0d16a5f95aa4fecd2723fc47d3cc24a
Bryson Tanner <sandwichman1618@gmail.com>
2024-12-17 09:09:39 -0600

    feat: Construct Eigen Vector Quantity explicitly

---
+++ include/poids/scalar_support/eigen_vector.hpp
@@ -0,0 +1,14 @@
+#ifndef POIDS_SCALAR_SUPPORT_EIGEN_VECTOR_HPP
+#define POIDS_SCALAR_SUPPORT_EIGEN_VECTOR_HPP
+
+#include <eigen3/Eigen/Core>
+
+#include "poids/core/quantity.hpp"
+#include "poids/core/reference.hpp"
+#include "poids/core/scalar_support.hpp"
+
+namespace poids::scalar {
+
+}
+
+#endif
--- test/eigen_support/test_vector_support.cpp
+++ test/eigen_support/test_vector_support.cpp
@@ -1,7 +1,14 @@
+#include <eigen3/Eigen/Core>
 #include <gtest/gtest.h>
 
-#include "poids/poids.hpp"
+#include "poids/kgms.hpp"
+#include "poids/scalar_support/eigen_vector.hpp"
 
-TEST(TestEigenSupport, ConstructQuantityVector) {
-    EXPECT_FALSE(false);
+TEST(TestEigenSupport, ConstructExplicitly) {
+  Eigen::Vector2d expected{3.4, 0.56};
+
+  auto actual = poids::Quantity<Eigen::Vector2d,
+                                kgms::units::AreaUnit>::makeFromBaseUnitValue({3.4, 0.56});
+
+  EXPECT_TRUE(expected.isApprox(actual.base(), 1e-6));
 }

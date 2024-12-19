84b87980a0132418f908916b6661ad122cf8039e
Bryson Tanner <sandwichman1618@gmail.com>
2024-12-18 21:43:21 -0600

    feat: Implement * and / for Vectors and Quantities

--- test/eigen_support/test_vector_arithmetic.cpp
+++ test/eigen_support/test_vector_arithmetic.cpp
@@ -31,3 +31,40 @@ TEST(TestVectorArithmetic, SubtractVectorVector) {
 
   EXPECT_TRUE(expected.isApprox(actual));
 }
+
+//! Vector * Vector multiplication can result in matrix types, which are not yet
+//! supported, so don't support this operation yet.
+// TODO: Implement Vector * Vector multiplication in the future.
+
+TEST(TestVectorArithmetic, MultiplyVectorQuantity) {
+  kgms::ForceVector<3> expected{Vector3d{2.0, 6.0, 12.0} * kilo(newton)};
+
+  kgms::AccelerationVector<3> a{Vector3d{1.0, 3.0, 6.0} * kilo(meter) / square(second)};
+  kgms::Mass b{2 * kilogram};
+
+  kgms::ForceVector<3> actual{a * b};
+
+  EXPECT_TRUE(expected.isApprox(actual));
+}
+
+TEST(TestVectorArithmetic, MultiplyQuantityVector) {
+  kgms::ForceVector<3> expected{Vector3d{-150.0, 60.0, 1.2} * kilo(newton)};
+
+  kgms::Mass b{-3 * kilogram};
+  kgms::AccelerationVector<3> a{Vector3d{50.0, -20.0, -0.4} * kilo(meter) / square(second)};
+
+  kgms::ForceVector<3> actual{a * b};
+
+  EXPECT_TRUE(expected.isApprox(actual));
+}
+
+TEST(TestVectorArithmetic, DivideVectorQuantity) {
+  kgms::VolumeVector<4> expected{Vector4d{10.0, 2.0, 30.0, 40.0} * meter3};
+
+  kgms::SecondMomentOfAreaVector<4> a{Vector4d{100.0, 20.0, 300.0, 400.0} * meter4};
+  kgms::Length b{10 * meter};
+
+  kgms::VolumeVector<4> actual{a / b};
+
+  EXPECT_TRUE(expected.isApprox(actual));
+}

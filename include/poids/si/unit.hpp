179a9b25fb3f106b017ad800d289db01484ad46f
Bryson Tanner <sandwichman1618@gmail.com>
2024-12-27 18:35:43 -0600

    feat: Add unit utilities for creating SI units

--- include/poids/si/unit.hpp
+++ include/poids/si/unit.hpp
@@ -20,8 +20,52 @@ namespace si {
     using amount = AmountRatio;
     using luminosity = LuminosityRatio;
 
+    template <typename Other>
+    using multiply_t = void;
+
     using unitless_t = si::UnitType<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>>;
   };
+
+  namespace detail {
+    template <intmax_t N, intmax_t D = 1>
+    using TimeUnit = UnitType<std::ratio<N, D>>;
+    template <intmax_t N, intmax_t D = 1>
+    using LengthUnit = UnitType<std::ratio<0>, std::ratio<N, D>>;
+    template <intmax_t N, intmax_t D = 1>
+    using MassUnit = UnitType<std::ratio<0>, std::ratio<0>, std::ratio<N, D>>;
+    template <intmax_t N, intmax_t D = 1>
+    using CurrentUnit = UnitType<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<N, D>>;
+    template <intmax_t N, intmax_t D = 1>
+    using TemperatureUnit = UnitType<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<N, D>>;
+    template <intmax_t N, intmax_t D = 1>
+    using AmountUnit = UnitType<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<N, D>>;
+    template <intmax_t N, intmax_t D = 1>
+    using LuminosityUnit = UnitType<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<N, D>>;
+
+    template <typename First, typename Second = void, typename... Others>
+    struct combine {
+      using type = typename combine<typename combine<First, Second>::type, Others...>::type;
+    };
+
+    template <typename Unit>
+    struct combine<Unit> {
+      using type = Unit;
+    };
+
+    template <typename First, typename Second>
+    struct combine<First, Second> {
+      using type = UnitType<std::ratio_add<typename First::time, typename Second::time>,
+                            std::ratio_add<typename First::length, typename Second::length>,
+                            std::ratio_add<typename First::mass, typename Second::mass>,
+                            std::ratio_add<typename First::current, typename Second::current>,
+                            std::ratio_add<typename First::temperature, typename Second::temperature>,
+                            std::ratio_add<typename First::amount, typename Second::amount>,
+                            std::ratio_add<typename First::luminosity, typename Second::luminosity>>;
+    };
+
+    template <typename... Units>
+    using combine_t = typename combine<Units...>::type;
+  }  // namespace detail
 }  // namespace si
 
 #endif
--- test/si/test_dimensional_analysis.cpp
+++ test/si/test_dimensional_analysis.cpp
@@ -113,4 +113,174 @@ TEST(TestSIDimension, UnitlessIsDefault) {
   EXPECT_TYPE_EQ(expected, actual);
 }
 
+TEST(TestSIDimension, EasyTime) {
+  using expected = si::UnitType<std::ratio<3, 4>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>>;
+
+  using actual = si::detail::TimeUnit<3, 4>;
+
+  EXPECT_TYPE_EQ(expected, actual);
+}
+
+TEST(TestSIDimension, EasyLength) {
+  using expected = si::UnitType<std::ratio<0>,
+                                std::ratio<7, 1>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>>;
+
+  using actual = si::detail::LengthUnit<7>;
+
+  EXPECT_TYPE_EQ(expected, actual);
+}
+
+TEST(TestSIDimension, EasyMass) {
+  using expected = si::UnitType<std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<2, 3>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>>;
+
+  using actual = si::detail::MassUnit<2, 3>;
+
+  EXPECT_TYPE_EQ(expected, actual);
+}
+
+TEST(TestSIDimension, EasyCurrent) {
+  using expected = si::UnitType<std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<1, 5>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>>;
+
+  using actual = si::detail::CurrentUnit<1, 5>;
+
+  EXPECT_TYPE_EQ(expected, actual);
+}
+
+TEST(TestSIDimension, EasyTemperature) {
+  using expected = si::UnitType<std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<6>,
+                                std::ratio<0>,
+                                std::ratio<0>>;
+
+  using actual = si::detail::TemperatureUnit<6, 1>;
+
+  EXPECT_TYPE_EQ(expected, actual);
+}
+
+TEST(TestSIDimension, EasyAmount) {
+  using expected = si::UnitType<std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<1, 2>,
+                                std::ratio<0>>;
+
+  using actual = si::detail::AmountUnit<1, 2>;
+
+  EXPECT_TYPE_EQ(expected, actual);
+}
+
+TEST(TestSIDimension, EasyLuminosity) {
+  using expected = si::UnitType<std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<5, 6>>;
+
+  using actual = si::detail::LuminosityUnit<5, 6>;
+
+  EXPECT_TYPE_EQ(expected, actual);
+}
+
+TEST(TestSIDimension, CombineSingle) {
+  using expected = si::UnitType<std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<5, 6>>;
+
+  using actual = si::detail::combine_t<si::detail::LuminosityUnit<5, 6>>;
+
+  EXPECT_TYPE_EQ(expected, actual);
+}
+
+TEST(TestSIDimension, CombineTwo) {
+  using expected = si::UnitType<std::ratio<-1, 2>,
+                                std::ratio<0>,
+                                std::ratio<2>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>>;
+
+  using actual = si::detail::combine_t<
+      si::detail::TimeUnit<-1, 2>,
+      si::detail::MassUnit<2>>;
+
+  EXPECT_TYPE_EQ(expected, actual);
+}
+
+TEST(TestSIDimension, CombineThree) {
+  using expected = si::UnitType<std::ratio<-1, 2>,
+                                std::ratio<0>,
+                                std::ratio<2>,
+                                std::ratio<0>,
+                                std::ratio<3>,
+                                std::ratio<0>,
+                                std::ratio<0>>;
+
+  using actual = si::detail::combine_t<
+      si::detail::TimeUnit<-1, 2>,
+      si::detail::TemperatureUnit<3>,
+      si::detail::MassUnit<2>>;
+
+  EXPECT_TYPE_EQ(expected, actual);
+}
+
+TEST(TestSIDimension, CombineMultiple) {
+  using si::detail::LengthUnit;
+  using si::detail::MassUnit;
+  using si::detail::TimeUnit;
+
+  using expected = si::UnitType<std::ratio<-3>,
+                                std::ratio<2>,
+                                std::ratio<3>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>,
+                                std::ratio<0>>;
+
+  using actual = si::detail::combine_t<TimeUnit<-1>,
+                                       TimeUnit<-1>,
+                                       TimeUnit<-1>,
+                                       LengthUnit<3>,
+                                       LengthUnit<-1>,
+                                       MassUnit<1>,
+                                       MassUnit<1>,
+                                       MassUnit<1>>;
+
+  EXPECT_TYPE_EQ(expected, actual);
+}
+
 #undef EXPECT_TYPE_EQ
